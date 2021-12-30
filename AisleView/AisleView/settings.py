e57ac52b2079d8058e90840f4556c8304b4105ac
Dalila K Leo <dalilakatialeo@gmail.com>
2021-12-30 16:09:41 +1000

    Update settings.py

--- AisleView/AisleView/settings.py
+++ AisleView/AisleView/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG') != 'False'
 
 ALLOWED_HOSTS = []
 
