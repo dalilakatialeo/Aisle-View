a1f5964c06a0d1ef1014004ee28588977683683c
Dalila K Leo <dalilakatialeo@gmail.com>
2021-12-30 23:08:01 +1000

    products

--- AisleView/users/models.py
+++ AisleView/users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class CustomUser (AbstractUser):
+class CustomUser(AbstractUser):
     pass
 
     def __str__(self):
